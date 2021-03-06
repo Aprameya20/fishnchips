3a48e063b50edde872dc70fe087d925b35801536
Ananya Aprameya <ananyaaprameya5@gmail.com>
2021-03-06 22:28:30 +0530

    Blockchain address added

--- builder/admin.py
+++ builder/admin.py
@@ -3,14 +3,26 @@ from . models import a_fleet,b_subfleet,c_wholesaler,d_retailer,e_consumer
 # Register your models here.
 
 class fleetAdmin(admin.ModelAdmin):
-    list_display = ('id',)
+    list_display = ('id','fishName','quantity','lat','lon','blockchain_address')
+
+class subfleetAdmin(admin.ModelAdmin):
+    list_display = ('id','fleetid','quantity','blockchain_address')
+
+class wholesalerAdmin(admin.ModelAdmin):
+    list_display = ('id','subfleetid','quantity','blockchain_address')
+
+class retailerAdmin(admin.ModelAdmin):
+    list_display = ('id','wholesalerid', 'quantity','blockchain_address')
+
+class consumerAdmin(admin.ModelAdmin):
+    list_display = ('id','retailerid','quantity','blockchain_address')
 
 
 admin.site.register(a_fleet,fleetAdmin)
-admin.site.register(b_subfleet)
-admin.site.register(c_wholesaler)
-admin.site.register(d_retailer)
-admin.site.register(e_consumer)
+admin.site.register(b_subfleet,subfleetAdmin)
+admin.site.register(c_wholesaler,wholesalerAdmin)
+admin.site.register(d_retailer,retailerAdmin)
+admin.site.register(e_consumer,consumerAdmin)
 
 admin.site.site_title ='Fish n Chips ADMIN'
 admin.site.site_header ="Fish n Chips Admin"
--- builder/models.py
+++ builder/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+import uuid
+import random
+#from datetime import datetime
+
+#now = datetime.now()
 import hashlib
 
 class a_fleet (models.Model):
@@ -7,27 +12,36 @@ class a_fleet (models.Model):
     quantity = models.IntegerField()
     type = models.CharField(max_length=20)
     timestamp = models.DateTimeField()
+    #word = now.strftime("%m/%d/%Y, %H:%M:%S")
     lat = models.FloatField()
     lon = models.FloatField()
     company = models.CharField(max_length=50)
+    #print(id)
+    #blockchain_addr = models.CharField(max_length=64,default=hashlib.sha256(str(id).encode('utf-8')).hexdigest())
+    blockchain_address = models.UUIDField(default=uuid.uuid4)
+
 
 class b_subfleet(models.Model):
     fleetid = models.ForeignKey(a_fleet, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     timestamp = models.DateTimeField()
+    blockchain_address = models.UUIDField(default=uuid.uuid4)
 
 
 class c_wholesaler(models.Model):
     subfleetid = models.ForeignKey(b_subfleet, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     timestamp = models.DateTimeField()
+    blockchain_address = models.UUIDField(default=uuid.uuid4)
 
 class d_retailer(models.Model):
     wholesalerid = models.ForeignKey(c_wholesaler, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     timestamp = models.DateTimeField()
+    blockchain_address = models.UUIDField(default=uuid.uuid4)
 
 class e_consumer(models.Model):
     retailerid = models.ForeignKey(d_retailer, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     timestamp = models.DateTimeField()
+    blockchain_address = models.UUIDField(default=uuid.uuid4)
